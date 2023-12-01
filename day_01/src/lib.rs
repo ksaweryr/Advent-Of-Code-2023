c680100726b5eb2183b3ebf39f4c7d8048848c8c
Ksawery Radziwiłowicz <ksawery.radziwilowicz@gmail.com>
2023-12-01 09:56:15 +0100

    Day 1 part 1

--- day_01/src/lib.rs
+++ day_01/src/lib.rs
@@ -1,3 +1,8 @@
 pub fn solve(input: String) {
-    println!("TBI");
+    let part1 = input
+        .lines()
+        .map(|line| line.chars().filter(char::is_ascii_digit).map(|c| c as u64 - '0' as u64).collect::<Vec<_>>())
+        .map(|nums| nums[0] * 10 + nums.last().unwrap()).sum::<u64>();
+
+    println!("{part1}");
 }
