e57e59e64952a270434d7bae76ace73b5c167642
Ksawery Radziwiłowicz <ksawery.radziwilowicz@gmail.com>
2023-12-09 08:14:05 +0100

    Day 9 part 2

--- day_09/src/lib.rs
+++ day_09/src/lib.rs
@@ -4,6 +4,7 @@ pub fn solve(input: String) {
     let lines = parse_input(&input);
 
     println!("{}", part1(&lines));
+    println!("{}", part2(&lines));
 }
 
 fn parse_input(input: &str) -> Vec<Vec<isize>> {
@@ -23,11 +24,23 @@ fn part1(lines: &Vec<Vec<isize>>) -> isize {
     lines.iter().map(next_value).sum()
 }
 
+fn part2(lines: &Vec<Vec<isize>>) -> isize {
+    lines.iter().map(previous_value).sum()
+}
+
 fn next_value(row: &Vec<isize>) -> isize {
     if row.iter().all(|x| x == &0) {
         0
     } else {
         row.last().unwrap() + next_value(&differences(&row))
+    }
+}
+
+fn previous_value(row: &Vec<isize>) -> isize {
+    if row.iter().all(|x| x == &0) {
+        0
+    } else {
+        row.first().unwrap() - previous_value(&differences(row))
     }
 }
 
@@ -44,6 +57,12 @@ mod test {
     fn example_part1() {
         let lines = parse_input(EXAMPLE_INPUT);
         assert_eq!(part1(&lines), 114);
+    }
+
+    #[test]
+    fn example_part2() {
+        let lines = parse_input(EXAMPLE_INPUT);
+        assert_eq!(part2(&lines), 2);
     }
 
     const EXAMPLE_INPUT: &str = "0 3 6 9 12 15
