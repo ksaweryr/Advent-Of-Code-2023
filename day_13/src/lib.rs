d919fb61e4be6d15f0f6655f20af4e60324c6c54
Ksawery Radziwiłowicz <ksawery.radziwilowicz@gmail.com>
2023-12-13 11:36:47 +0100

    Day 13 part 2

--- day_13/src/lib.rs
+++ day_13/src/lib.rs
@@ -1,28 +1,29 @@
 pub fn solve(input: String) {
     let maps = parse_input(&input);
 
-    println!("{}", part1(&maps));
+    println!("{}", get_solution(&maps, 1));
+    println!("{}", get_solution(&maps, 2));
 }
 
-fn part1(maps: &Vec<Vec<Vec<char>>>) -> usize {
-    maps.iter().map(value).sum()
+fn get_solution(maps: &Vec<Vec<Vec<char>>>, part: usize) -> usize {
+    maps.iter().map(|m| value(m, part - 1)).sum()
 }
 
 fn parse_input(input: &str) -> Vec<Vec<Vec<char>>> {
     input.split("\n\n").map(|block| block.lines().map(|line| line.chars().collect()).collect()).collect()
 }
 
-fn value(map: &Vec<Vec<char>>) -> usize {
-    if let Some(col) = (1..map.len()).find(|col| is_reflection_line(&map, *col)) {
+fn value(map: &Vec<Vec<char>>, expected_distance: usize) -> usize {
+    if let Some(col) = (1..map.len()).find(|col| is_reflection_line(&map, *col, expected_distance)) {
         100 * col
     } else {
         let transposed = transposed(&map);
-        (1..transposed.len()).find(|row| is_reflection_line(&transposed, *row)).unwrap()
+        (1..transposed.len()).find(|row| is_reflection_line(&transposed, *row, expected_distance)).unwrap()
     }
 }
 
-fn is_reflection_line(map: &Vec<Vec<char>>, line: usize) -> bool {
-    map[0..line].iter().rev().zip(map[line..].iter()).all(|(r1, r2)| r1 == r2)
+fn is_reflection_line(map: &Vec<Vec<char>>, line: usize, expected_distance: usize) -> bool {
+    map[0..line].iter().rev().zip(map[line..].iter()).map(|(r1, r2)| distance(r1, r2)).sum::<usize>() == expected_distance
 }
 
 fn transposed(map: &Vec<Vec<char>>) -> Vec<Vec<char>> {
@@ -32,6 +33,10 @@ fn transposed(map: &Vec<Vec<char>>) -> Vec<Vec<char>> {
     (0..w).map(|x| (0..h).map(move |y| map[y][x]).collect()).collect()
 }
 
+fn distance(lhs: &Vec<char>, rhs: &Vec<char>) -> usize {
+    lhs.iter().zip(rhs).filter(|(a, b)| a != b).count()
+}
+
 #[cfg(test)]
 mod tests {
     use crate::*;
@@ -39,19 +44,31 @@ mod tests {
     #[test]
     fn test_is_reflection_line() {
         let map = &parse_input(EXAMPLE_INPUT)[1];
-        assert!(is_reflection_line(map, 4));
+        assert!(is_reflection_line(map, 4, 0));
     }
 
     #[test]
     fn test_value_vertical() {
         let map = &parse_input(EXAMPLE_INPUT)[0];
-        assert_eq!(value(map), 5);
+        assert_eq!(value(map, 0), 5);
     }
 
     #[test]
     fn test_value_horizontal() {
         let map = &parse_input(EXAMPLE_INPUT)[1];
-        assert_eq!(value(map), 400);
+        assert_eq!(value(map, 0), 400);
+    }
+
+    #[test]
+    fn example_part1() {
+        let maps = parse_input(EXAMPLE_INPUT);
+        assert_eq!(get_solution(&maps, 1), 405);
+    }
+
+    #[test]
+    fn example_part2() {
+        let maps = parse_input(EXAMPLE_INPUT);
+        assert_eq!(get_solution(&maps, 2), 400);
     }
 
     const EXAMPLE_INPUT: &str = "#.##..##.
