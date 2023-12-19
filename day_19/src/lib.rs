2d58119db05ff8e975725a65b0afe8a0f3961e86
Ksawery Radziwiłowicz <ksawery.radziwilowicz@gmail.com>
2023-12-19 15:28:03 +0100

    Day 19 part 2

--- day_19/src/lib.rs
+++ day_19/src/lib.rs
@@ -3,9 +3,10 @@ use std::{collections::HashMap, str::FromStr};
 use anyhow::Error;
 
 pub fn solve(input: String) {
-    let (system, parts) = parse_input(&input);
+    let (mut system, parts) = parse_input(&input);
 
     println!("{}", part1(&system, &parts));
+    println!("{}", part2(&mut system));
 }
 
 fn parse_input(input: &str) -> (System, Vec<Part>) {
@@ -19,6 +20,10 @@ fn parse_input(input: &str) -> (System, Vec<Part>) {
 
 fn part1(system: &System, parts: &Vec<Part>) -> usize {
     parts.iter().filter(|p| system.is_accepted(*p)).map(|p| p.value()).sum()
+}
+
+fn part2(system: &mut System) -> usize {
+    system.eval_workflow("in").iter().map(|pr| pr.elements()).sum()
 }
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
@@ -57,15 +62,22 @@ impl RatingCategory {
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 enum Comparison {
-    LT,
-    GT
+    LT(usize),
+    GT(usize)
 }
 
 impl Comparison {
-    fn test(&self, lhs: usize, rhs: usize) -> bool {
+    fn test(&self, lhs: usize) -> bool {
         match self {
-            Self::LT => lhs < rhs,
-            Self::GT => lhs > rhs
+            Self::LT(rhs) => lhs < *rhs,
+            Self::GT(rhs) => lhs > *rhs
+        }
+    }
+
+    fn inverted(&self) -> Self {
+        match self {
+            Self::LT(rhs) => Self::GT(rhs - 1),
+            Self::GT(rhs) => Self::LT(rhs + 1)
         }
     }
 }
@@ -93,21 +105,53 @@ impl FromStr for RuleTarget {
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 enum Rule {
-    Conditional(RatingCategory, Comparison, usize, RuleTarget),
+    Conditional(RatingCategory, Comparison, RuleTarget),
     Unconditional(RuleTarget)
 }
 
 impl Rule {
     fn eval(&self, part: &Part) -> Option<RuleTarget> {
         match self {
-            Self::Conditional(rc, cmp, value, target) => {
-                if cmp.test(rc.get(part), *value) {
+            Self::Conditional(rc, cmp, target) => {
+                if cmp.test(rc.get(part)) {
                     Some(target.clone())
                 } else {
                     None
                 }
             },
             Self::Unconditional(target) => Some(target.clone())
+        }
+    }
+
+    fn fallthrough_range(&self) -> PartRange {
+        if let Self::Conditional(cat, cmp, _) = self {
+            return <PartRange as Default>::default().bound(cat, &cmp.inverted()).unwrap();
+        }
+
+        panic!("No fallthrough range for unconditional rule");
+    }
+
+    fn eval_range(&self, system: &mut System) -> Vec<PartRange> {
+        match self {
+            Self::Conditional(cat, cmp, target) => {
+                match target {
+                    RuleTarget::Accept => match <PartRange as Default>::default().bound(cat, cmp) {
+                        Some(x) => vec![x],
+                        None => vec![]
+                    },
+                    RuleTarget::Reject => vec![],
+                    RuleTarget::Next(name) => {
+                        system.eval_workflow(name).iter().filter_map(|pr| pr.bound(cat, cmp)).collect()
+                    }
+                }
+            },
+            Self::Unconditional(target) => {
+                match target {
+                    RuleTarget::Accept => vec![Default::default()],
+                    RuleTarget::Reject => vec![],
+                    RuleTarget::Next(name) => system.eval_workflow(name).clone(),
+                }
+            }
         }
     }
 }
@@ -118,9 +162,9 @@ impl FromStr for Rule {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         if let Some((cond, target)) = s.split_once(":") {
             if let Some((cat, val)) = cond.split_once("<") {
-                Ok(Self::Conditional(cat.parse()?, Comparison::LT, val.parse()?, target.parse()?))
+                Ok(Self::Conditional(cat.parse()?, Comparison::LT(val.parse()?), target.parse()?))
             } else if let Some((cat, val)) = cond.split_once(">") {
-                Ok(Self::Conditional(cat.parse()?, Comparison::GT, val.parse()?, target.parse()?))
+                Ok(Self::Conditional(cat.parse()?, Comparison::GT(val.parse()?), target.parse()?))
             } else {
                 Err(Error::msg(format!("Invalid operator in `{}`", cond)))
             }
@@ -140,6 +184,30 @@ impl Workflow {
     fn eval(&self, part: &Part) -> RuleTarget {
         self.rules.iter().find_map(|r| r.eval(part)).expect("No rule matched")
     }
+
+    fn eval_range(&self, system: &mut System) -> Vec<PartRange> {
+        self.rules.iter().scan((<PartRange as Default>::default(), true), |(state, should_continue), rule| {
+            if !*should_continue {
+                return None;
+            }
+
+            let result = rule.eval_range(system).iter().filter_map(|x| x.intersection(state)).collect::<Vec<_>>();
+
+            if let Rule::Unconditional(_) = rule {
+                *should_continue = false;
+            } else {
+                let new_state = state.intersection(&rule.fallthrough_range());
+
+                if let Some(st) = new_state {
+                    *state = st
+                } else {
+                    *should_continue = false;
+                }
+            }
+
+            Some(result)
+        }).flatten().collect()
+    }
 }
 
 impl FromStr for Workflow {
@@ -156,7 +224,14 @@ impl FromStr for Workflow {
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 struct System {
-    workflows: HashMap<String, Workflow>
+    workflows: HashMap<String, Workflow>,
+    evaluations: HashMap<String, Vec<PartRange>>
+}
+
+impl System {
+    fn new(workflows: HashMap<String, Workflow>) -> Self {
+        Self { workflows, evaluations: HashMap::new() }
+    }
 }
 
 impl FromStr for System {
@@ -170,7 +245,7 @@ impl FromStr for System {
             }
         }).collect::<Result<HashMap<String, Workflow>, _>>()?;
 
-        Ok(System { workflows })
+        Ok(System::new(workflows))
     }
 }
 
@@ -185,6 +260,16 @@ impl System {
             RuleTarget::Reject => false,
             RuleTarget::Next(name) => self._is_accepted(part, self.workflows.get(&name).unwrap().eval(part))
         }
+    }
+
+    fn eval_workflow(&mut self, name: &str) -> &Vec<PartRange> {
+        if self.evaluations.get(name).is_none() {
+            let workflow = self.workflows.get(name).unwrap().clone();
+            let new_evaluation = workflow.eval_range(self);
+            self.evaluations.insert(name.to_owned(), new_evaluation);
+        }
+
+        self.evaluations.get(name).unwrap()
     }
 }
 
@@ -216,6 +301,73 @@ impl FromStr for Part {
     }
 }
 
+#[derive(Clone, Copy, Debug, Default, Eq, PartialEq)]
+struct PartRange {
+    xrange: Range,
+    mrange: Range,
+    arange: Range,
+    srange: Range
+}
+
+impl PartRange {
+    fn intersection(&self, other: &PartRange) -> Option<PartRange> {
+        let xrange = self.xrange.intersection(&other.xrange)?;
+        let mrange = self.mrange.intersection(&other.mrange)?;
+        let arange = self.arange.intersection(&other.arange)?;
+        let srange = self.srange.intersection(&other.srange)?;
+
+        Some(PartRange { xrange, mrange, arange, srange })
+    }
+
+    fn bound(&self, cat: &RatingCategory, cmp: &Comparison) -> Option<Self> {
+        match cat {
+            RatingCategory::X => Some(Self { xrange: self.xrange.intersection(&Range::from(cmp))?, ..*self }),
+            RatingCategory::M => Some(Self { mrange: self.mrange.intersection(&Range::from(cmp))?, ..*self }),
+            RatingCategory::A => Some(Self { arange: self.arange.intersection(&Range::from(cmp))?, ..*self }),
+            RatingCategory::S => Some(Self { srange: self.srange.intersection(&Range::from(cmp))?, ..*self }),
+        }
+    }
+
+    fn elements(&self) -> usize {
+        self.xrange.elements() * self.mrange.elements() * self.arange.elements() * self.srange.elements()
+    }
+}
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+struct Range {
+    min: usize,
+    max: usize
+}
+
+impl Range {
+    fn intersection(&self, other: &Range) -> Option<Range> {
+        if self.min > other.max || other.min > self.max {
+            None
+        } else {
+            Some(Range { min: self.min.max(other.min), max: self.max.min(other.max) })
+        }
+    }
+
+    fn elements(&self) -> usize {
+        self.max - self.min + 1
+    }
+}
+
+impl Default for Range {
+    fn default() -> Self {
+        Self { min: 1, max: 4000 }
+    }
+}
+
+impl From<&Comparison> for Range {
+    fn from(value: &Comparison) -> Self {
+        match value {
+            Comparison::GT(a) => Self { min: a + 1, ..Default::default() },
+            Comparison::LT(b) => Self { max: b - 1, ..Default::default() }
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use crate::*;
@@ -232,6 +384,13 @@ mod tests {
         let (system, parts) = parse_input(EXAMPLE_INPUT);
 
         assert_eq!(part1(&system, &parts), 19114);
+    }
+
+    #[test]
+    fn example_part2() {
+        let (mut system, _) = parse_input(EXAMPLE_INPUT);
+
+        assert_eq!(part2(&mut system), 167409079868000);
     }
 
     const EXAMPLE_INPUT: &str = "px{a<2006:qkq,m>2090:A,rfg}
